b2bcae96fb4bcf1b5cf2a7cc630e47b450a4bb88
Dimitrijevs <dimitrijevgleb@gmail.com>
2025-07-24 14:22:03 +0300

    api docs from gateway

--- src/main/java/com/microservices/microservice_gateway/routes/ProductRoutes.java
+++ src/main/java/com/microservices/microservice_gateway/routes/ProductRoutes.java
@@ -11,26 +11,41 @@ import org.springframework.web.servlet.function.RequestPredicates;
 import org.springframework.web.servlet.function.RouterFunction;
 import org.springframework.web.servlet.function.ServerResponse;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class ProductRoutes {
 
-    // create
-    @Bean
-    public RouterFunction<ServerResponse> createProductServiceRoute() {
-        return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.path("/api/product/create"),
-                        HandlerFunctions.http())
-                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8081")))
-                .build();
-    }
+        // create
+        @Bean
+        public RouterFunction<ServerResponse> createProductServiceRoute() {
+                return GatewayRouterFunctions.route("product_service")
+                                .route(RequestPredicates.path("/api/product/create"),
+                                                HandlerFunctions.http())
+                                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8081")))
+                                .build();
+        }
 
-    // all
-    @Bean
-    public RouterFunction<ServerResponse> allProductsServiceRoute() {
-        return GatewayRouterFunctions.route("product_service")
-                .route(RequestPredicates.path("/api/product/all"),
-                        HandlerFunctions.http())
-                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8081")))
-                .build();
-    }
+        // all
+        @Bean
+        public RouterFunction<ServerResponse> allProductsServiceRoute() {
+                return GatewayRouterFunctions.route("product_service")
+                                .route(RequestPredicates.path("/api/product/all"),
+                                                HandlerFunctions.http())
+                                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8081")))
+                                .build();
+        }
+
+        @Bean
+        public RouterFunction<ServerResponse> docsProductsServiceRoute() {
+                return GatewayRouterFunctions.route("product_service")
+                                .route(RequestPredicates.path("/aggregate/product-service/v3/api-docs"),
+                                                HandlerFunctions.http())
+                                .before(BeforeFilterFunctions.uri(URI.create("http://localhost:8081")))
+                                .before(BeforeFilterFunctions.rewritePath(
+                                                "/aggregate/product-service/v3/api-docs",
+                                                "/api-docs"))
+                                .build();
+        }
 }
